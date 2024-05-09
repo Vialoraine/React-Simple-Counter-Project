fdbb6f6d46d66b03b44fd57fef4074ced6fdca11
Loreina Salalila <vialoraine.gg@gmail.com>
2024-05-09 05:04:44 -0700

    created states and button functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import "./App.css";
 import { React, useState } from "react";
 
+// increase - this will increment the value
+// decrease - this will decrement the value
+// reset - to reset the counter back to zero.
+ 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  //increase counter
+  const increase = () => {
+    setCounter((count) => count + 1);
+  };
+
+  //decrease counter
+  const decrease = () => {
+    setCounter((count) => count - 1);
+  };
+
+  //reset counter
+  const reset = () => {
+    setCounter(0);
+  };
+
   return (
     <div className="counter">
       <h1>Using React: A Simple Counter</h1>
-      <span className="counter__output"></span>
+      <span className="counter__output">{counter}</span>
       <div className="btn__container">
-        <button className="control__btn">+</button>
-        <button className="control__btn">-</button>
-        <button className="reset">Reset</button>
+        <button className="control__btn" onClick={increase}>
+          +
+        </button>
+        <button className="control__btn" onClick={decrease}>
+          -
+        </button>
+        <button className="reset" onClick={reset}>
+          Reset
+        </button>
       </div>
     </div>
   );

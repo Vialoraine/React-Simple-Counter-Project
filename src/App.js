80c9f00cbd6da08417b876dc92eeca30cc7375dd
Loreina Salalila <vialoraine.gg@gmail.com>
2024-05-09 05:01:05 -0700

    created the return elements for the counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import './App.css';
+import "./App.css";
 import { React, useState } from "react";
 
 export default function App() {
   return (
- 
-  )
+    <div className="counter">
+      <h1>Using React: A Simple Counter</h1>
+      <span className="counter__output"></span>
+      <div className="btn__container">
+        <button className="control__btn">+</button>
+        <button className="control__btn">-</button>
+        <button className="reset">Reset</button>
+      </div>
+    </div>
+  );
 }
